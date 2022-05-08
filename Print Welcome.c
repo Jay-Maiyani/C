c4bb730cd46013f307f2d80b2e4cb109c8b38f72
Jay Maiyani <70792601+Jay-Maiyani@users.noreply.github.com>
2022-05-08 16:44:57 +0530

    Create Print Welcome.c

---
+++ Print Welcome.c
@@ -0,0 +1,8 @@
+//First C Program
+#include <stdio.h> // for including print function
+
+int main() 
+{
+  printf("Welcome\n"); //this is print function and to print anything this need to be used
+  return 0;
+}
