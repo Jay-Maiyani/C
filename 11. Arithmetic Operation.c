181aec3bb0fb93a01fa173c6331c4a6eaee453d8
Jay Maiyani <70792601+Jay-Maiyani@users.noreply.github.com>
2022-05-08 21:37:20 +0530

    Create 11. Arithmetic Operation.c

---
+++ 11. Arithmetic Operation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+int n1,n2; //performing various arithmetic operation
+printf("Enter first number = \n");
+scanf("%d",&n1);
+printf("Enter second number = \n");
+scanf("%d",&n2);
+printf("The sum of %d and %d is %d\n",n1,n2,n1+n2);
+printf("The difference of %d and %d is %d\n",n1,n2,n1-n2);
+printf("The product of %d and %d is %d\n",n1,n2,n1*n2);
+printf("The quotient of %d and %d is %d\n",n1,n2,n1/n2);
+printf("The remainder of %d and %d is %d \n",n1,n2,n1%n2);
+}
