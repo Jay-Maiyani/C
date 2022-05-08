c038ba9eace25914d9da78932f8c76f406a2aca7
Jay Maiyani <70792601+Jay-Maiyani@users.noreply.github.com>
2022-05-08 21:47:32 +0530

    Create 18. To perform increment and decrement operation.c

---
+++ 18. To perform increment and decrement operation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int n1,n2,n3;
+printf("Enter first number: ");
+scanf("%d",&n1);
+printf("Enter second number: ");
+scanf("%d",&n2);
+printf("Enter third number: ");
+scanf("%d",&n3);
+printf("++a = %d \n", ++n1);
+printf("a = %d \n", n1);
+printf("a++ = %d \n", n1++);
+printf("a = %d \n", n1);
+printf("--b = %d \n", --n2);
+printf("++c = %d \n", ++n3);
+}
