a417a7d11ee83aad043321b6af6e31600eb5c5e0
Jay Maiyani <70792601+Jay-Maiyani@users.noreply.github.com>
2022-05-08 21:30:43 +0530

    Create 9. First n natural numbers.c

---
+++ 9. First n natural numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+int n,i;
+printf("How many numbers to display = ");
+scanf("%d",&n);
+for(i=1;i<=n;i++) // this is for loop and i++ is increment of number
+    {printf("%d\n",i);}
+}
